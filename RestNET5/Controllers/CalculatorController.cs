2b3fad4d50bff254a29272adeed3e3b8662899f0
Douglas Samuel <douglassam007@gmail.com>
2022-01-17 15:14:39 -0300

    Adding the calculator operations.

---
+++ RestNET5/Controllers/CalculatorController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace RestNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Values");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDouble(firstNumber) - ConvertToDouble(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid Values");
+        }
+
+        [HttpGet("mul/{firstNumber}/{secondNumber}")]
+        public IActionResult Mul(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mul = ConvertToDouble(firstNumber) * ConvertToDouble(secondNumber);
+                return Ok(mul.ToString());
+            }
+            return BadRequest("Invalid Values");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if (double.Parse(secondNumber) == 0)
+                    return BadRequest("Division by 0");
+
+                var div = ConvertToDouble(firstNumber) / ConvertToDouble(secondNumber);
+                return Ok(div.ToString());
+            }
+            return BadRequest("Invalid Values");
+        }
+
+        [HttpGet("med/{firstNumber}/{secondNumber}")]
+        public IActionResult Med(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var med = (ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber)) / 2;
+                return Ok(med.ToString());
+            }
+            return BadRequest("Invalid Values");
+        }
+
+        [HttpGet("rai/{firstNumber}")]
+        public IActionResult Rai(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var rai = Math.Sqrt(ConvertToDouble(firstNumber));
+                return Ok(rai.ToString());
+            }
+            return BadRequest("Invalid Values");
+        }
+
+        private static double ConvertToDouble(string strNumber)
+        {
+            return double.Parse(strNumber);
+        }
+
+        private static bool IsNumeric(string strNumber)
+        {
+            return double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out double number);
+        }
+    }
+}
--- RestNET5/Startup.cs
+++ RestNET5/Startup.cs
@@ -1,16 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestNET5
 {
@@ -28,10 +20,6 @@ namespace RestNET5
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestNET5", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +28,6 @@ namespace RestNET5
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestNET5 v1"));
             }
 
             app.UseHttpsRedirection();
