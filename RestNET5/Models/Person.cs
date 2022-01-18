fe3cd7fde7016c62d23f2ee5318197f0fb48bc2e
Douglas Samuel <douglassam007@gmail.com>
2022-01-18 08:46:42 -0300

    Changing calculator controller to person controller.

---
+++ RestNET5/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestNET5.Models
+{
+    public class Person
+    {
+        public double Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
